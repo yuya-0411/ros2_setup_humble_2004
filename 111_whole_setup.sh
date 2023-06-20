4dd873f610e2dfd32db677f2c961a6c429be9c55
yuya-0411 <pzgq5uh7@s.okayama-u.ac.jp>
2023-06-20 11:36:41 +0900

    whole setup from 1 to 7

---
+++ 111_whole_setup.sh
@@ -0,0 +1,7 @@
+bash 001_set_locale.sh
+bash 002_ros2_apt_repository
+bash 003_install_dev_tools.sh
+bash 004_ros2_code.sh
+bash 005_install_dependency.sh
+bash 006_build_code.sh
+bash 007_env_setup.sh
