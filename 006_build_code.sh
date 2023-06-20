a051ba9c856e6e2674e5bd008ac554b7476bb4e0
yuya-0411 <pzgq5uh7@s.okayama-u.ac.jp>
2023-06-20 16:25:58 +0900

    for fast build

--- 006_build_code.sh
+++ 006_build_code.sh
@@ -1,4 +1,5 @@
 cd ~/ros2_humble/
 export MAKEFLAGS=-j1
-colcon build --symlink-install --parallel-workers 1
+# colcon build --symlink-install --parallel-workers 1
+colcon build --symlink-install
 cd ~/ros2_setup_humble_2004
