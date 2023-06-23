cab4ca108728ee33e57d48c00fa3425d620828ed
yuya-0411 <83892604+yuya-0411@users.noreply.github.com>
2023-06-24 02:42:37 +0900

    add -y

--- 001_set_locale.sh
+++ 001_set_locale.sh
@@ -1,6 +1,6 @@
 locale  # check for UTF-8
 
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
