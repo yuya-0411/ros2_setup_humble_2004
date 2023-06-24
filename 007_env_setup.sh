d72a34b8cfd9c0297f6ba3e0296b550a4f808c6d
yuya-0411 <83892604+yuya-0411@users.noreply.github.com>
2023-06-24 10:22:15 +0900

    write down setting in bashrc

--- 007_env_setup.sh
+++ 007_env_setup.sh
@@ -1,3 +1,3 @@
 # Replace ".bash" with your shell if you're not using bash
 # Possible values are: setup.bash, setup.sh, setup.zsh
-. ~/ros2_humble/install/local_setup.bash
+echo "source ~/ros2_humble/install/local_setup.bash" >> ~/.bashrc
