53518a0c38d01f7dac039ebe128c153edeb4d1c2
yuya-0411 <83892604+yuya-0411@users.noreply.github.com>
2023-06-24 09:51:25 +0900

    add skip command

--- 006_build_code.sh
+++ 006_build_code.sh
@@ -1,4 +1,4 @@
 cd ~/ros2_humble/
 export MAKEFLAGS=-j1
-colcon build --symlink-install --parallel-workers 1
+colcon build --symlink-install --parallel-workers 1 --continue-on-error --packages-skip-build-finished
 cd ~/ros2_setup_humble_2004
